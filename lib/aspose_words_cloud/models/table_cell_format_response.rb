00a9aa50247a034319b44930c5a3bf68e49188a8
wordscloud <aspose.cloud@aspose.com>
2020-02-20 08:05:55 -0800

    SDK regenerated by CI server [ci skip]

--- lib/aspose_words_cloud/models/table_cell_format_response.rb
+++ lib/aspose_words_cloud/models/table_cell_format_response.rb
@@ -29,7 +29,7 @@ module AsposeWordsCloud
  # --------------------------------------------------------------------------------------------------------------------
  #
 
-  # This response should be returned by the service when handling: GET http://api.aspose.com/v4.0/words/Test.doc/tables/{0}/rows/{1}/cells/{2}/cellformat.
+  # This response should be returned by the service when handling: GET https://api.aspose.cloud/v4.0/words/Test.doc/tables/{0}/rows/{1}/cells/{2}/cellformat.
   class TableCellFormatResponse
     # Gets or sets request Id.
     attr_accessor :request_id
