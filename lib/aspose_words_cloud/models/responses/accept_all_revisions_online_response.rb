41e63ef545e2f23486122354a898747c020af740
jenkins <jenkins.aspose@gmail.com>
2026-01-16 09:55:43 -0600

    SDK regenerated by CI server [ci skip]

--- lib/aspose_words_cloud/models/responses/accept_all_revisions_online_response.rb
+++ lib/aspose_words_cloud/models/responses/accept_all_revisions_online_response.rb
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------------
 # <copyright company="Aspose" file="accept_all_revisions_online_response.rb">
-#   Copyright (c) 2025 Aspose.Words for Cloud
+#   Copyright (c) 2026 Aspose.Words for Cloud
 # </copyright>
 # <summary>
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
